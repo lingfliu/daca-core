2e25d5d9b2b0e640ee68121068dd878f23ef99fe
lingfliu <lingfeng.liu@163.com>
2021-04-05 00:22:02 +0800

    inited vertx

---
+++ src/main/java/xyz/issc/daca/servers/vertx/SocketVert.java
@@ -0,0 +1,17 @@
+package xyz.issc.daca.servers.vertx;
+
+import xyz.issc.daca.NioChannel;
+import xyz.issc.daca.NioServer;
+
+public class SocketVert extends NioServer {
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+}
